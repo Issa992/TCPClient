72d5e2279e36442ca2f5b19e66d8e3d4e7ffaea3
Issa <abdu2997@edu.easj.dk>
2018-09-24 21:16:48 +0200

    finish

--- TCPClient/Program.cs
+++ TCPClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,52 @@ namespace TCPClient
 {
     class Program
     {
+        private static TcpClient _clientSocket = null;
+        private static Stream stream = null;
+        private static StreamWriter Writer = null;
+        private static StreamReader Reader = null;
         static void Main(string[] args)
         {
+            try
+            {
+                using (_clientSocket=new TcpClient("127.0.0.1", 7))
+                {
+                    using (stream = _clientSocket.GetStream())
+                    {
+                        while (true)
+                        {
+                            Writer = new StreamWriter(stream) { AutoFlush = true };
+                           
+                            Console.WriteLine("Write your request here ");
+                            string clientMsg = Console.ReadLine();
+
+                            Writer.WriteLine(clientMsg);
+
+                            Reader = new StreamReader(stream);
+                            string rdMsgFromServer = Reader.ReadLine();
+                            if (rdMsgFromServer != null)
+                            {
+                                Console.WriteLine("Client recieved Message from Server:" + rdMsgFromServer);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Client recieved null message from Server ");
+                            }
+                            Console.WriteLine("Press enter to stop the client!");
+                            Console.ReadKey();
+                        }
+                    }
+                }
+            
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
         }
+
     }
+
 }
